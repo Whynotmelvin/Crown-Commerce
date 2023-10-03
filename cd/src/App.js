8693a5b90d9b54136c73841a2b03900ab3e516cc
Melvin <melvinundercover@icloud.com>
2023-10-03 11:00:36 +0100

    Scafolding Complete

---
+++ cd/src/App.js
@@ -0,0 +1,44 @@
+const App = () => {
+  return (
+  <div className="categories-container">
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-conatiner">
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-conatiner">
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-conatiner">
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-conatiner">
+        <h2>Womens</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-conatiner">
+        <h2>Mens</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default App;
+ 
