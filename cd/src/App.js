70d1625f1e952b5a8bf12b71ffa12c6bfc7ce374
Melvin <melvinundercover@icloud.com>
2023-10-16 11:27:20 +0100

    Deleted package.json outside cd

--- cd/src/App.js
+++ cd/src/App.js
@@ -1,17 +1,19 @@
-import Home from "./routes/home/home.components"
-import {
-  createBrowserRouter
-} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
+import Home from './routes/home/home.components';
 
-const Router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Home/>
-  },
-]);
+const Shop =() =>{
+  return <h1>I am the shop page</h1>;
+}
 
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/'  element={<Home />}>
+        <Route path='shop' element ={<Shop/>}></Route>
+      </Route>
+    </Routes>
+  );
+};
 
-export default Router;
-
-
+export default App;
